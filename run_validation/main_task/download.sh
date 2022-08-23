a30c7078f532412275249de7cad9068c5ba1579c
Andrew Ramsay <andrewramsay@users.noreply.github.com>
2022-08-23 13:30:10 +0100

    Update script name

--- run_validation/main_task/download.sh
+++ run_validation/main_task/download.sh
@@ -7,4 +7,4 @@ wget -c https://cast-y4-collection.s3.amazonaws.com/all_hashes.csv -P files
 wget -c https://raw.githubusercontent.com/daltonj/treccastweb/master/2022/2022_evaluation_topics_turn_ids.json -P files
 
 echo "Building database"
-python3 hash_db.py -b 20000 -i 100000 files/all_hashes.csv
+python3 passage_id_db.py -b 20000 -i 100000 files/all_hashes.csv
