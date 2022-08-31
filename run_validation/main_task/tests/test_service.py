79275d29fc7191a72505a7698eac0a11886372c0
Andrew Ramsay <andrewramsay@users.noreply.github.com>
2022-08-31 18:10:17 +0100

    adding a test using multiple simultaneous clients

--- run_validation/main_task/tests/test_service.py
+++ run_validation/main_task/tests/test_service.py
@@ -1,6 +1,11 @@
+import time
+import multiprocessing
+import random
+
 import pytest
 
 from passage_validator import PassageValidator
+from main import validate
 
 def test_service_startup(servicer_params_test):
     pv = PassageValidator(*servicer_params_test)
@@ -12,3 +17,26 @@ def test_service_startup_invalid_rows(servicer_params_test):
 
     assert(pytest_exc.type == SystemExit)
     assert(pytest_exc.value.code == 255)
+
+def validate_wrapper(run_file, file_root, max_warnings, skip_validation, strict, start_delay):
+    time.sleep(start_delay)
+    turns_validated, warning_count, service_errors = validate(run_file, file_root, max_warnings, skip_validation, strict)
+    return (turns_validated, warning_count, service_errors)
+
+@pytest.mark.slow
+def test_service_multiple_clients(default_validate_args, grpc_server_full):
+    num_clients = 25
+    args = default_validate_args
+
+    validation_args = [(args.path_to_run_file,
+                        args.fileroot,
+                        args.max_warnings,
+                        args.skip_passage_validation,
+                        args.strict,
+                        random.random()) for x in range(num_clients)]
+
+    with multiprocessing.Pool(processes=num_clients) as pool:
+        results = pool.starmap(validate_wrapper, validation_args)
+
+    for i in range(num_clients):
+        assert(results[i] == (205, 0, 1))
